51cf2ad04a616c2e4e13044be2be9ef78886351a
SuperNova <hwchon1234@naver.com>
2016-02-13 20:42:15 +0900

    first commit

---
+++ SamplePopup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SamplePopup
+//
+//  Created by Sung on 2016. 2. 13..
+//  Copyright © 2016년 self. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
